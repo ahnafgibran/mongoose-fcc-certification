1611047fa2f92d82cb55365c4522d8b5e8970ca1
Ahnaf Gibran <ahnafgibran@gmail.com>
2023-09-20 01:27:20 +0700

    modify removeManyPeople handler

--- myApp.js
+++ myApp.js
@@ -94,8 +94,12 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
+  Person.remove({ name: nameToRemove }, (err, response) => {
+    if(err) return console.log(err);
+    done(null, response);
+  })
 
-  done(null /*, data*/);
+  // done(null /*, data*/);
 };
 
 const queryChain = (done) => {
