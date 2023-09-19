d9ab16f9a9f88e775f48fbdf0af6219f8d3455c0
Ahnaf Gibran <ahnafgibran@gmail.com>
2023-09-19 16:07:17 +0700

    add mongodb connection

--- myApp.js
+++ myApp.js
@@ -1,5 +1,14 @@
 require('dotenv').config();
 
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
+  if (err) {
+    console.log('Database error: ' + err);
+  } else {
+    console.log('Successful database connection');
+  }
+});
 
 let Person;
 
