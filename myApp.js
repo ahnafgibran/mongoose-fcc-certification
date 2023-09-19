85bb7befd8e3c4481ea7fdc0be012cbee2aab5c6
Ahnaf Gibran <ahnafgibran@gmail.com>
2023-09-19 17:34:18 +0700

    Modify findPersonById handler

--- myApp.js
+++ myApp.js
@@ -49,7 +49,11 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if (err) return console.error(err);
+    done(null, data);
+  })
+  // done(null /*, data*/);
 };
 
 const findEditThenSave = (personId, done) => {
